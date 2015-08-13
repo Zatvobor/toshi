b46d0c349af150b179c5e3b4c32c57b65104954e
Zatvor <a.zatvornitskiy@gmail.com>
2015-08-13 19:16:30 +0300

    Tiny fix

--- lib/toshi/web/api.rb
+++ lib/toshi/web/api.rb
@@ -44,11 +44,11 @@ module Toshi
         opts  = Toshi::Utils.sanitize_options(opts)
         where = {branch: opts[:branch]} if opts[:branch]
 
-        @blocks = Toshi::BlocksLogic.all(where, opts[:limit], opts[:offset])
+        blocks = Toshi::BlocksLogic.all(where, opts[:limit], opts[:offset])
 
         case format
         when 'json'
-          json @blocks.map(&:to_hash)
+          json blocks.map(&:to_hash)
         when 'rss'
           builder :blocks_rss
         else
@@ -58,23 +58,23 @@ module Toshi
 
       # get latest block or search by hash or height
       get '/blocks/:block.?:format?' do
-        @block = Toshi::BlocksLogic.first_by_hash_or_latest_by_time!(params[:block], params[:time])
+        block = Toshi::BlocksLogic.first_by_hash_or_latest_by_time!(params[:block], params[:time])
 
         case format
-        when 'json'; json(@block.to_hash)
-        when 'hex';  @block.raw.payload.unpack("H*")[0]
-        when 'bin';  @block.raw.payload
+        when 'json'; json(block.to_hash)
+        when 'hex';  block.raw.payload.unpack("H*")[0]
+        when 'bin';  block.raw.payload
         else raise InvalidFormatError
         end
       end
 
       # get block transactions
       get '/blocks/:block/transactions.?:format?' do
-        @block = Toshi::BlocksLogic.first_by_hash_or_latest_by_time!(params[:block], params[:time])
+        block = Toshi::BlocksLogic.first_by_hash_or_latest_by_time!(params[:block], params[:time])
 
         case format
         when 'json'
-          json(@block.to_hash({show_txs: true, offset: params[:offset], limit: params[:limit]}))
+          json(block.to_hash({show_txs: true, offset: params[:offset], limit: params[:limit]}))
         else
           raise InvalidFormatError
         end
