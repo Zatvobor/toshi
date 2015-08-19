c37cc97885b01a9b1307c2d856f3753fd54ec312
Zatvor <a.zatvornitskiy@gmail.com>
2015-08-19 17:05:48 +0300

    Move both 'from_time' and 'from_range' methods from Block to BlocksLogic

--- lib/toshi/logics/blocks_logic.rb
+++ lib/toshi/logics/blocks_logic.rb
@@ -7,6 +7,15 @@ module Toshi
     @block = Toshi::Models::Block
     @nfe   = Toshi::Web::Base::NotFoundError
 
+    def first_in_range(range, order=Sequel.method(:desc))
+      from_range(0..range, order).first
+    end
+
+    def from_range(range, order=Sequel.method(:desc))
+      key = range.first < 500_000_000 ? :height : :time
+      @block.order(order.call(key)).where(key => range)
+    end
+
     def all(where, limit=nil, offset=nil)
       query = @block.order(Sequel.desc(:id))
       query = query.method(:where).call(where) if where
@@ -21,7 +30,7 @@ module Toshi
 
     def first_by_hash_or_latest_by_time(ask, time=nil)
       if ask.to_s == 'latest'
-        time ? @block.from_time(time.to_i) : @block.head
+        time ? first_in_range(time.to_i) : @block.head
       elsif ask.to_s.size < 64 && (Integer(ask) rescue false)
         @block.where(height: ask, branch: 0).first
       else
@@ -31,7 +40,7 @@ module Toshi
 
     def last_by_time(time)
       time = Time.now if !time || time.to_i == 0
-      @block.from_time(time.to_i)
+      first_in_range(time.to_i)
     end
 
     def all_in_period(year, month, mday)
@@ -48,7 +57,7 @@ module Toshi
 
     def all_in_range(start_period, end_period)
       period = start_period.to_time.utc.to_i..end_period.to_time.utc.to_i
-      @block.from_range(period)
+      from_range(period)
     end
 
   end
